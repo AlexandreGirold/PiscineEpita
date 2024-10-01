dbcd853d20b92332e4cc7e2cf9c830964211e3c5
Alexandre <92374888+AlexandreGirold@users.noreply.github.com>
2024-10-01 19:36:42 +0200

    Create int_sqrt.c

---
+++ piscine/my_pow/int_sqrt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int int_sqrt(int n) {
+    if (n < 0) {
+        return -1;
+    }
+    
+    int i = 0;
+    while (i * i <= n) {
+        i++;
+    }
+    
+    return i - 1;
+}
+
