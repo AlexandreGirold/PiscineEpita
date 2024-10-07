5873f25caf305667c9d0ff1656b230552bf52c25
Alexandre <92374888+AlexandreGirold@users.noreply.github.com>
2024-10-07 12:01:40 +0200

    Update freq_analysis.c

--- piscine/freq_analysis/freq_analysis.c
+++ piscine/freq_analysis/freq_analysis.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
-#include <string.h>
+#include <stddef.h>
 
 void freq_analysis(const char text[], const char table[]) 
 {
     int freq[26] = {0}; 
-    int length = strlen(text);
+    int len = 0;
 
-
-    for (int i = 0; i < length; i++) {
-        if (text[i] >= 'A' && text[i] <= 'Z') {
-            freq[text[i] - 'A']++;
-        }
+    
+    while (text[len] != '\0') 
+    {
+        if (text[len] >= 'A' && text[len] <= 'Z')
+            freq[text[len] - 'A']++;
+        len++;
     }
+
 
     char letters[26];
     int counts[26];
     int uniqueCount = 0;
 
-    for (int i = 0; i < 26; i++) {
-        if (freq[i] > 0) {
-            letters[uniqueCount] = 'A' + i;
+
+    for (int i = 0; i < 26; i++) 
+    {
+        if (freq[i] > 0) 
+        {
+            letters[uniqueCount] = 'A' + i; 
             counts[uniqueCount] = freq[i];
             uniqueCount++;
         }
     }
-    for (int i = 0; i < uniqueCount - 1; i++) {
-        for (int j = i + 1; j < uniqueCount; j++) {
-            if (counts[i] < counts[j]) {
-                
+
+    for (int i = 0; i < uniqueCount - 1; i++) 
+    {
+        for (int j = i + 1; j < uniqueCount; j++) 
+        {
+            if (counts[i] < counts[j]) 
+            {
+
                 int tempCount = counts[i];
                 counts[i] = counts[j];
                 counts[j] = tempCount;
@@ -39,15 +48,24 @@ void freq_analysis(const char text[], const char table[])
         }
     }
 
-    char mapping[26] = {0};
-    for (int i = 0; i < uniqueCount; i++) {
-        mapping[letters[i] - 'A'] = table[i];
-    }
 
-    for (char c = 'A'; c <= 'Z'; c++) {
-        if (mapping[c - 'A'] != 0) {
-            printf("%c %c$\n", mapping[c - 'A'], c); // Added '$' for expected output
-        }
-    }
+    char mapping[26] = {0};
+    for (int i = 0; i < uniqueCount; i++)
+        mapping[letters[i] - 'A'] = table[i];
+
+
+    for (char c = 'A'; c <= 'Z'; c++)
+        if (mapping[c - 'A'] != 0)
+            printf("%c %c\n", mapping[c - 'A'], c);
 }
 
+int main()
+{
+    const char text[] = "ABBCCCDDDDD"; 
+
+    const char table[] = "ABCD"; 
+
+    freq_analysis(text, table);
+
+    return 0;
+}
