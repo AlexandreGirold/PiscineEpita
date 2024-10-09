4d237d47acbbcbb9ea8d3bfa6d21bbbff6e2c376
Alexandre <92374888+AlexandreGirold@users.noreply.github.com>
2024-10-09 11:12:43 +0200

    Update freq_analysis.c

--- piscine/freq_analysis/freq_analysis.c
+++ piscine/freq_analysis/freq_analysis.c
@@ -1,53 +1,49 @@
-#include <stddef.h>
 #include <stdio.h>
-void freq_analysis(const char text[], const char table[])
-{
-    int freq[26] = { 0 };
+#include <assert.h>
+
+void freq_analysis(const char text[], const char table[]) {
+    int freq[26] = {0};
     int len = 0;
 
-    while (text[len] != '\0')
-    {
-        if (text[len] >= 'A' && text[len] <= 'Z')
-            freq[text[len] - 'A']++;
+    // Count frequency of each character in the text and calculate length
+    while (text[len] != '\0') {
+        freq[text[len] - 'A']++;
         len++;
     }
 
-    char letters[26];
-    int counts[26]; // fonctionne avec letters (compte la freq de lettre dans
-                    // letters)
-    int uniqueCount = 0;
-
-    for (int i = 0; i < 26; i++)
-    {
-        if (freq[i] > 0)
-        {
-            letters[uniqueCount] = 'A' + i;
-            counts[uniqueCount] = freq[i];
-            uniqueCount++;
-        }
+    // Create an array of characters sorted by frequency
+    char sorted_chars[26];
+    for (int i = 0; i < 26; i++) {
+        sorted_chars[i] = 'A' + i;
     }
 
-    for (int i = 0; i < uniqueCount - 1; i++)
-    {
-        for (int j = i + 1; j < uniqueCount; j++)
-        {
-            if (counts[i] < counts[j])
-            {
-                int tempCount = counts[i];
-                counts[i] = counts[j];
-                counts[j] = tempCount;
-
-                char tempLetter = letters[i];
-                letters[i] = letters[j];
-                letters[j] = tempLetter;
+    // Sort the characters based on frequency
+    for (int i = 0; i < 25; i++) {
+        for (int j = i + 1; j < 26; j++) {
+            if (freq[sorted_chars[i] - 'A'] < freq[sorted_chars[j] - 'A']) {
+                char temp = sorted_chars[i];
+                sorted_chars[i] = sorted_chars[j];
+                sorted_chars[j] = temp;
             }
         }
     }
 
-    char mapping[26] = { 0 };
-    for (int i = 0; i < uniqueCount; i++)
-        mapping[letters[i] - 'A'] = table[i];
+    // Print the associations in alphabetical order of the ciphered letters
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            // Find the position of 'A' + i in sorted_chars
+            int pos = 0;
+            while (sorted_chars[pos] != 'A' + i) {
+                pos++;
+            }
+            printf("%c %c\n", 'A' + i, table[pos]);
+        }
+    }
+}
 
-    for (int i = 0; i < uniqueCount; i++)
-        printf("%c %c\n", mapping[letters[i] - 'A'], letters[i]);
+int main() {
+    freq_analysis("ABBCCCDDDDD", "ABCD");
+    printf("\n");
+    freq_analysis("AAB", "XY");
+    return 0;
 }
