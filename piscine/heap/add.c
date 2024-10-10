4672b8e62199c0de80eee9cd82107434e76eeba6
Alexandre <92374888+AlexandreGirold@users.noreply.github.com>
2024-10-10 15:21:49 +0400

    Create add.c

---
+++ piscine/heap/add.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "heap.h"
+#include <assert.h>
+
+static void swap(int *a, int *b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+static void heapify_up(struct heap *heap, size_t index) 
+{
+    while (index > 0) {
+        size_t parent = (index - 1) / 2;
+        if (heap->array[parent] >= heap->array[index]) break;
+        swap(&heap->array[parent], &heap->array[index]);
+        index = parent;
+    }
+}
+
+void add(struct heap *heap, int val) 
+{
+    if (heap->size >= heap->capacity) {
+        heap->capacity *= 2;
+        heap->array = realloc(heap->array, heap->capacity * sizeof(int));
+    }
+    heap->array[heap->size] = val;
+    heapify_up(heap, heap->size);
+    heap->size++;
+}
